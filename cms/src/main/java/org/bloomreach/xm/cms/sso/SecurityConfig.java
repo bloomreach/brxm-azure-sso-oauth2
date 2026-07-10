9bc7300c0260accb17aa40fdb19ee53843d3b455
David Bailey <david.bailey@bloomreach.com>
2026-07-10 13:54:41 +0200

    Upgrade to 16.9.3

--- cms/src/main/java/org/bloomreach/xm/cms/sso/SecurityConfig.java
+++ cms/src/main/java/org/bloomreach/xm/cms/sso/SecurityConfig.java
@@ -3,59 +3,64 @@ package org.bloomreach.xm.cms.sso;
 import static org.bloomreach.xm.cms.sso.SsoConstants.LOCAL_LOGIN_ENABLED;
 import static org.bloomreach.xm.cms.sso.SsoConstants.LOCAL_LOGIN_HEADER;
 
-import com.azure.spring.cloud.autoconfigure.aad.AadWebSecurityConfigurerAdapter;
+import com.azure.spring.cloud.autoconfigure.implementation.aad.security.AadWebApplicationHttpSecurityConfigurer;
 import com.google.common.base.Strings;
 import java.io.IOException;
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.LogoutConfigurer;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.web.access.intercept.FilterSecurityInterceptor;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.access.intercept.AuthorizationFilter;
 import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
 import org.springframework.security.web.util.matcher.RequestHeaderRequestMatcher;
 
 /**
  * Security configuration to place desired endpoints behind auth.
  */
+@Configuration
 @EnableWebSecurity
-@EnableGlobalMethodSecurity(prePostEnabled = true)
-public class SecurityConfig extends AadWebSecurityConfigurerAdapter {
+@EnableMethodSecurity
+public class SecurityConfig {
 
   private static final Logger log = LoggerFactory.getLogger(SecurityConfig.class);
 
-  @Override
-  protected void configure(HttpSecurity http) throws Exception {
-    super.configure(http);
+  @Bean
+  public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    http.with(AadWebApplicationHttpSecurityConfigurer.aadWebApplication(), Customizer.withDefaults());
 
     //check if SSO is enabled
     String sso = System.getenv(SsoConstants.SSO_ENABLED);
-    if (!Strings.isNullOrEmpty(sso) && Boolean.valueOf(sso)) {
+    if (!Strings.isNullOrEmpty(sso) && Boolean.parseBoolean(sso)) {
       log.info("SSO is enabled - securing CMS endpoints.");
       http.authorizeHttpRequests(authorize -> authorize
           //utility servlets/endpoints are not secured, they have their own internal auth
-          .antMatchers(new String[]{
+          .requestMatchers(
               "/ws/indexexport", "/ping",
               "/repository/**",
               "/angular/**", "/skin/**", "/ckeditor/**", "/**.svg",
               "/logging/**",
               "/ws/**",
-              SsoConstants.LOGOUT_URL //used by Azure for logging out
-          }).permitAll()
+              SsoConstants.LOGOUT_URL) //used by Azure for logging out
+          .permitAll()
           //allow local login via request headers
           .requestMatchers(new RequestHeaderRequestMatcher(LOCAL_LOGIN_HEADER, LOCAL_LOGIN_ENABLED)).permitAll()
           //everyting else is secured
           .anyRequest().authenticated());
-      http.addFilterAfter(new LoginFilter(), FilterSecurityInterceptor.class);
+      http.addFilterAfter(new LoginFilter(), AuthorizationFilter.class);
 
       //logout handler to forward user to Azure's logout page
-      http.logout().logoutSuccessHandler(new LogoutSuccessHandler() {
+      http.logout(logout -> logout.logoutSuccessHandler(new LogoutSuccessHandler() {
         @Override
         public void onLogoutSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication)
             throws IOException, ServletException {
@@ -64,12 +69,12 @@ public class SecurityConfig extends AadWebSecurityConfigurerAdapter {
           if (!localLogin) {
             //the JSP uses Javascript to break out of the iframing done by the navapp
             String url = "https://login.microsoftonline.com/" + System.getenv("SSO_TENANT_ID") + "/oauth2/v2.0/logout";
-            request.setAttribute("logoutUrl", url.toString());
+            request.setAttribute("logoutUrl", url);
             request.getRequestDispatcher(SsoConstants.LOGOUT_JSP).forward(request, response);
             response.flushBuffer();
           }
         }
-      });
+      }));
     } else {
       log.info("SSO is disabled.");
       http.authorizeHttpRequests(authorize -> authorize
@@ -77,8 +82,10 @@ public class SecurityConfig extends AadWebSecurityConfigurerAdapter {
     }
 
     //CSRF not supported by CMS
-    http.csrf().disable();
+    http.csrf(AbstractHttpConfigurer::disable);
     //same origin needed by CMS
-    http.headers().frameOptions().sameOrigin();
+    http.headers(headers -> headers.frameOptions(HeadersConfigurer.FrameOptionsConfig::sameOrigin));
+
+    return http.build();
   }
 }
